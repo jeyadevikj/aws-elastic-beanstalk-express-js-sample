6946c1fb34bcc5708300c0343f9d6cc7bb284385
Jeya Devi Jeevankumar <jeyadevi.jeevankumar@bounteous.com>
2023-06-02 16:01:55 +0530

    Full pipeline tested

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welocome All....!!!'));
+app.get('/', (req, res) => res.send('updated!!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
